5c62dc5513c6047a9c6eb3abc76b9293bea312a6
Marcelo8173 <marceloandrebio@gmail.com>
2020-12-05 15:39:03 -0300

    end to header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,20 +1,30 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import Logo from '../../assets/Logo.svg';
 import {Container} from './styles';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+import {GiDogBowl} from 'react-icons/gi';
+
 
 const Header: React.FC = () => {
+    const history = useHistory();
+
+    const handleDahsboard = useCallback(() =>{
+        history.push('/');
+    },[history]);
+
     return(
         <Container>
             <div className="logo-container">
                 <img src={Logo} alt="findPet" width="120px"/>
                 <div>
-                    <button>Pagina inicial</button>
+                    <button onClick={handleDahsboard}>Pagina inicial</button>
                     <button>Hoje</button>
                 </div>
             </div>
             <div className="bell" >
-                <Link to="/following">Sino</Link>
+                <Link to="/following">
+                    <GiDogBowl size={40} color="#FFBD1D"/>
+                </Link>
             </div>
         </Container>
     )
