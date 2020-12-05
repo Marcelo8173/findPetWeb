1abc510d6a6acd500a9e6b7cddcff0038e481245
Marcelo8173 <marceloandrebio@gmail.com>
2020-12-05 14:40:33 -0300

    create struct of Following

---
+++ src/pages/Following/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Following: React.FC = () => {
+    return(
+        <div>Seguindo</div>
+    );
+}
+
+export default Following;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch} from 'react-router-dom';
 import Dashboard from '../pages/Dashboard/index';
+import Following from '../pages/Following';
 
 const Routes = () => {
     return(
         <BrowserRouter>
             <Switch>
                 <Route path="/" exact component={Dashboard}/>
+                <Route path="/following" exact component={Following} />
             </Switch>
         </BrowserRouter>
     )
