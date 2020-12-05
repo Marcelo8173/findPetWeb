f321843a670a45fd40002c7ab2a030639fd26d9e
Marcelo8173 <marceloandrebio@gmail.com>
2020-12-05 14:35:42 -0300

    create struct of dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import GlobalStyle from './styles/globalStyles';
+import Router from './routes/index';
 
 const App: React.FC = () => {
   return (
     <>
       <GlobalStyle />
+      <Router />
     </>
   );
 }
---
+++ src/routes/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch} from 'react-router-dom';
+import Dashboard from '../pages/Dashboard/index';
+
+const Routes = () => {
+    return(
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact component={Dashboard}/>
+            </Switch>
+        </BrowserRouter>
+    )
+}
+
+export default Routes;
