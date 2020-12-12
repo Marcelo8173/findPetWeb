e1b8dd3152aa55cd658b5f49ccdac0529d3b4bc2
Marcelo8173 <marceloandrebio@gmail.com>
2020-12-12 16:19:31 -0300

    criando estrutura dos meus favoritos

--- src/pages/Following/index.tsx
+++ src/pages/Following/index.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import HeaderContainer from '../../components/header';
-import {Container} from './styles';
+import {Container,CardContainer} from './styles';
 
 const Following: React.FC = () => {
     return(
         <Container>
             <HeaderContainer />
-            carrinho
+            <h1>Meus favoritos</h1>
+            <CardContainer>
+                <div>
+                    <img src="https://s2.glbimg.com/slaVZgTF5Nz8RWqGrHRJf0H1PMQ=/0x0:800x450/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2019/U/e/NTegqdSe6SoBAoQDjKZA/cachorro.jpg" alt=""/>
+                    <p>Nino</p>
+                    <p>Disponivel para adoção</p>
+                    <section>
+                        <button>Entrar em contato</button>
+                        <button>Remover dos favoritos</button>
+                    </section>
+                </div>
+            </CardContainer>
         </Container>
     );
 }
--- src/pages/Following/styles.ts
+++ src/pages/Following/styles.ts
@@ -3,4 +3,32 @@ import styled from 'styled-components';
 export const Container = styled.div`
     width: 100%;
     height: 100vh;
+    h1{
+        margin: 45px 35px;
+        color: #454545;
+        font-weight: 700;
+    }
+`;
+
+export const CardContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    margin: 5px 60px;
+    /* align-items: center;
+    justify-content: center;
+    padding: 10px; */
+    div{
+        display: flex;
+        align-items: center;
+        border: 1px solid black;
+        border-radius: 5px;
+        background: #FFFF;
+        padding: 25px;
+        img{
+            max-width: 100px;
+            height: auto;
+            border-radius: 5px;
+        }
+    }
+
 `;
