b277ab6e3f74073dd994dd169bf2f972d82d444b
Marcelo8173 <marceloandrebio@gmail.com>
2020-12-17 20:31:21 -0300

    criando rota para login

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,12 +3,14 @@ import { BrowserRouter, Route, Switch} from 'react-router-dom';
 import Dashboard from '../pages/Dashboard/index';
 import Following from '../pages/Following';
 import Details from '../pages/Details/index';
+import Login from '../pages/Login';
 
 const Routes = () => {
     return(
         <BrowserRouter>
             <Switch>
-                <Route path="/" exact component={Dashboard}/>
+                <Route path="/" exact component={Login} />
+                <Route path="/dashboard" exact component={Dashboard}/>
                 <Route path="/following" exact component={Following} />
                 <Route path="/details/:id" exact component={Details} />
             </Switch>
