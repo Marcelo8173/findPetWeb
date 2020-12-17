56d6f2f8697ca61d9884f58fab97982f81f34cab
Marcelo8173 <marceloandrebio@gmail.com>
2020-12-16 23:01:01 -0300

    criando addToFollow

--- src/hooks/Following.tsx
+++ src/hooks/Following.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, createContext, useCallback } from 'react';
+import React, { useContext, createContext, useCallback, useState, useEffect } from 'react';
 import { IItems } from '../pages/Dashboard';
 
 interface FollowContextData{
@@ -9,11 +9,35 @@ const FollowingContext = createContext<FollowContextData>({} as FollowContextDat
 
 
 export const Following: React.FC = ({children}) => {
-    const addToFollow = useCallback((item:IItems) => {
-        console.log(item)
-    },[])
     
-        return(
+    const [itemsAdd, setItemsAdd] = useState<IItems[]>([]);
+    
+    useEffect(() => {
+        const items = localStorage.getItem('@findPet: item'); 
+        if(items !== null){
+            setItemsAdd(JSON.parse(items));
+        }
+    },[])
+
+    const addToFollow = useCallback((item:IItems) => {
+        const arrayOfPet = itemsAdd;
+        let equals = arrayOfPet.find(items => items.id === item.id);
+       
+        if(equals){
+            console.log('existe');
+            return;
+        }
+        
+        arrayOfPet.push(item);
+
+        setItemsAdd(arrayOfPet);
+
+        localStorage.setItem('@findPet: item', JSON.stringify(item));
+
+        return arrayOfPet;
+    },[itemsAdd]);
+
+    return(
         <FollowingContext.Provider value={{addToFollow}}>
             {children}
         </FollowingContext.Provider>
