a491c5c6724151148f4fb48074185bd16798efea
Marcelo8173 <marceloandrebio@gmail.com>
2020-12-20 17:59:22 -0300

    style do login-input

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,5 +1,4 @@
 import React, { useCallback } from 'react';
-import boyandcat from '../../assets/boyandcat.png'
 import Logo from '../../assets/Logo.svg';
 import {Container,Content,AnimationContainer,Background} from './styles';
 import { useHistory, Link } from 'react-router-dom';
@@ -15,14 +14,12 @@ const Login: React.FC = () => {
             <Content>
                 <AnimationContainer>
                     <img src={Logo} alt="Logo GoBarber"/>
-                        <h1>Faça seu logon</h1>
-                        <input type="text"/>
-                        <input type="text"/>
-                        <button onClick={handleToLogin}>asd</button>
+                        <h1>Login</h1>
+                        <input placeholder="E-mail" type="text"/>
+                        <input placeholder="Senha" type="password"/>
+                        <button onClick={handleToLogin}>Entrar</button>
                         <Link to="/recuperar_senha">Esqueci minha senha</Link>
-                    <Link to="/cadastrar">
-                        
-                        Criar conta</Link>
+                    
                 </AnimationContainer>
             </Content>
             <Background />
--- src/pages/Login/styles.ts
+++ src/pages/Login/styles.ts
@@ -15,7 +15,7 @@ export const Content = styled.div`
     align-items: center;
     justify-content: center;
     width: 100%;
-    max-width: 700px;
+    max-width: 750px;
 `;
 
 const appearFromLeft = keyframes`
@@ -34,17 +34,21 @@ export const AnimationContainer = styled.div`
     flex-direction: column;
     align-items: center;
     justify-content: center;
-
+    width: 70%;
     animation: ${appearFromLeft} 1s;
 
-      form{
-        margin:  50px 0;
-        width: 340px;
-        text-align:center;
-
         h1{
-            margin-bottom:24px;
+            margin: 24px 0;
+            font-family: 'Roboto Slab';
         }   
+
+        input{
+            padding: 6px;
+            margin-bottom: 15px;
+            width: 100%;
+            border: 2px solid #878787;
+            border-radius: 5px;
+        }
 
         button{
             background: #ff9000;
@@ -55,7 +59,6 @@ export const AnimationContainer = styled.div`
             color: #312e38;
             width: 100%;
             font-weight: 500;
-            margin-top: 16px;
             transition: background 0.2s;
 
            &:hover{
@@ -74,7 +77,7 @@ export const AnimationContainer = styled.div`
             color: ${shade(0.2, '#F4EDE8')};
         }
 
-    }
+    
 }
 > a{
     color: #ff9000;
